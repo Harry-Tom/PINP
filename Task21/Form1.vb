8d7c524c819ffd1bb38df4bc8b3f32c6e58e56bd
Duckliffe <duckliffe@gmail.com>
2013-07-02 00:08:37 +0100

    Licensing stuff

--- Task21/Form1.vb
+++ Task21/Form1.vb
@@ -1,3 +1,18 @@
+' This file is part of PINP.
+
+' PINP is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+
+' PINP is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+
+' You should have received a copy of the GNU General Public License
+' along with PINP.  If not, see <http://www.gnu.org/licenses/>.
+
 ﻿Public Class Form1
     Dim spray, fill, drawing, saved As Boolean
     Dim pen As Boolean = True
